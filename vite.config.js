e7751e4fef1ab6b0284e597869a3ad5156bcff87
Deepanshu <deepakyt8033@gmail.com>
2026-07-06 09:30:28 +0530

    feat: create About page with Copernicus details and dynamic tag/stats scroll animations

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         contact: resolve(__dirname, "contact.html"),
+        about: resolve(__dirname, "about.html"),
       },
     },
     assetsInclude: [
